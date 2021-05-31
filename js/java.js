81bd325309560930abb179c61a61077c1c0fafc8
Manuel Cerdeira <manuel.c2893@gmail.com>
2021-05-31 19:49:43 -0300

    primer commit

---
+++ js/java.js
@@ -0,0 +1,14 @@
+"use strict"
+
+document.querySelector('.menu_bar')=document.addEventListener('click', mostrar);
+
+function mostrar(){
+    let nav=document.querySelector('nav');
+    nav.addEventListener('visibility: visible');
+}
+
+
+/*var nav=document.querySelector('nav');
+nav.addEventListener('click', function(){
+    nav.classList.toogle('mostrar');
+})*/
