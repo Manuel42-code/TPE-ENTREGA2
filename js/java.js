20a599da6ee737de3295846514daf262fc331d3a
Manuel Cerdeira <manuel.c2893@gmail.com>
2021-06-01 19:56:38 -0300

    nuevo commit

--- js/java.js
+++ js/java.js
@@ -1,14 +1,29 @@
 "use strict"
 
-document.querySelector('.menu_bar')=document.addEventListener('click', mostrar);
-
-function mostrar(){
-    let nav=document.querySelector('nav');
-    nav.addEventListener('visibility: visible');
+function Captcha() {
+    var alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+    var i;
+    for (i = 0; i < 6; i++) {
+        var a = alpha[Math.floor(Math.random() * alpha.length)];
+        var b = alpha[Math.floor(Math.random() * alpha.length)];
+        var c = alpha[Math.floor(Math.random() * alpha.length)];
+        var d = alpha[Math.floor(Math.random() * alpha.length)];
+        var e = alpha[Math.floor(Math.random() * alpha.length)];
+        var f = alpha[Math.floor(Math.random() * alpha.length)];
+        var g = alpha[Math.floor(Math.random() * alpha.length)];
+    }
+    var code = a+b+c+d+e+f+g;
+    document.getElementById('captcha').innerHTML=code;
+       
 }
 
-
-/*var nav=document.querySelector('nav');
-nav.addEventListener('click', function(){
-    nav.classList.toogle('mostrar');
-})*/
+function ValidarCaptcha() {
+    if (  document.getElementById('captcha').innerHTML === document.getElementById('txtInput').value) {
+       document.getElementById('resultado').innerHTML= "Captcha correcto.";
+    }
+    else {
+        document.getElementById('resultado').innerHTML= "Captcha Incorrecto. Vuelva a ingresarlo.";
+        Captcha();
+       
+    }
+}
